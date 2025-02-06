fb051867d3092e22a490475b00dafd44e4398bbb
adityaranjan858 <adityaranjan858@gmail.com>
2025-02-06 18:12:56 +0530

    changed button design

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,13 +4,13 @@ import PropTypes  from 'prop-types';
 const Pagination = ({pagenums, currentPageNo, pagenohandler, prevBtnHandler, nextBtnHandler}) => {
   return (
     <div className=''>
-      <button disabled={currentPageNo == 0} className={`border m-1 px-3 rounded-full font-medium ${currentPageNo == 0 ? " bg-gray-400": " bg-blue-400"}`} onClick={prevBtnHandler}><i className="fa-solid fa-chevron-left"></i></button>
+      <button disabled={currentPageNo == 0} className={` m-1 `} onClick={prevBtnHandler}><i className={`${currentPageNo == 0 ? " text-gray-400": " text-blue-400"} fa-solid fa-xl fa-circle-chevron-left`}></i></button>
         {[...Array(pagenums).keys()].map(num=>{
             return(
               <button key={num} className={`${num == currentPageNo ? "border m-1 px-3 rounded-full bg-blue-400":"border m-1 px-3 rounded-full font-medium"} `} onClick={()=> pagenohandler(num)}>{num + 1}</button>
             )
         })} 
-      <button disabled={ currentPageNo == pagenums - 1} className={`border m-1 px-3 rounded-full font-medium ${currentPageNo == pagenums - 1 ? " bg-gray-400": " bg-blue-400"}`} onClick={nextBtnHandler}><i className="fa-solid fa-chevron-right"></i></button>
+      <button disabled={ currentPageNo == pagenums - 1} className={`m-1`} onClick={nextBtnHandler}><i className={`${currentPageNo == pagenums - 1 ? " text-gray-400": " text-blue-400"} fa-solid fa-xl fa-circle-chevron-right `}></i></button>
     </div>
   )
 }
